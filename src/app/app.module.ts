37aebb22cbb590339f92afba16b686d57aa19803
Ankush <ankushgoyal11@gmail.com>
2019-05-29 14:35:32 -0400

    Added Pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BlotPostTileComponent } from './blot-post-tile/blot-post-tile.component';
 import { BlogListComponent } from './blog-list/blog-list.component';
+import { TruncatePipe } from './truncate.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     BlotPostTileComponent,
-    BlogListComponent
+    BlogListComponent,
+    TruncatePipe
   ],
   imports: [
     BrowserModule,
